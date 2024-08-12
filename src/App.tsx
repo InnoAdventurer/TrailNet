609961abd502fb4daf194814b4a85d11be34c128
DarrenIsNotNoob <darren535605@hotmail.com>
2024-08-12 17:38:52 +1000

    Completed 5 frontend pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WelcomePage from './Page/WelcomePage/WelcomePage';
 import WelcomeSubpage from './Page/WelcomeSubpage/WelcomeSubpage';
+import SignUpPage from './Page/SignUpPage/SignUpPage'
+import LoginPage from './Page/LoginPage/LoginPage'
+import PasswordPage from './Page/PasswordPage/PasswordPage';
+
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WelcomePage />}/>
-          <Route path="/WelcomeSubpage" element={<WelcomeSubpage />}/>
+          <Route path="/welcomesubpage" element={<WelcomeSubpage />}/>
+          <Route path="/signuppage" element={<SignUpPage />}/>
+          <Route path="/loginpage" element={<LoginPage />}/>
+          <Route path="/passwordpage" element={<PasswordPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
