a9b42ca5d1ec849d990cd008014f7c1d2dfa64e0
OceanNawshad <reshad947@gmail.com>
2024-08-19 07:43:31 -0700

    Add files via upload
    this is the first initial iteration of the database. Only has a single user table with some dummy data

---
+++ DB.sql
@@ -0,0 +1,6 @@
+INSERT INTO Users (username, email, password_hash, first_name, last_name, profile_picture)
+VALUES
+    ('johndoe', 'johndoe@example.com', 'hashed_password_1', 'John', 'Doe', 'path/to/johndoe.jpg'),
+    ('janedoe', 'janedoe@example.com', 'hashed_password_2', 'Jane', 'Doe', 'path/to/janedoe.jpg'),
+    ('bobsmith', 'bobsmith@example.com', 'hashed_password_3', 'Bob', 'Smith', 'path/to/bobsmith.jpg'),
+    ('alicejones', 'alicejones@example.com', 'hashed_password_4', 'Alice', 'Jones', 'path/to/alicejones.jpg');
