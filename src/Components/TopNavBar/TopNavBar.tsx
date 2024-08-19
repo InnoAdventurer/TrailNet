fb4cdaee38babbd78977b36127df4eea315aaac6
DarrenIsNotNoob <darren535605@hotmail.com>
2024-08-20 00:06:55 +1000

    HomePage is created

---
+++ src/Components/TopNavBar/TopNavBar.tsx
@@ -0,0 +1,21 @@
+import './TopNavBar.css';
+import { IoIosWarning } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoPersonAddOutline } from "react-icons/io5";
+import { GoBell } from "react-icons/go";
+
+function TopNavBar(){
+    return (
+      <header className="homepage-container flex">
+        <div className="header">
+            <IoIosWarning className="icon"/>
+            <IoSettingsOutline className="icon"/>
+            <h2>Home</h2>
+            <IoPersonAddOutline className="icon"/>
+            <GoBell className="icon"/>
+        </div>
+    </header>
+)
+}
+
+export default TopNavBar;
