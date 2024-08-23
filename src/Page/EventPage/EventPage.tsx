1de8b38f224958d9f9d8bd6e5f9511c191a5d336
DarrenIsNotNoob <darren535605@hotmail.com>
2024-08-23 16:06:14 +1000

    Built eventpage

--- src/Page/EventPage/EventPage.tsx
+++ src/Page/EventPage/EventPage.tsx
@@ -1,44 +1,62 @@
 import './EventPage.css';
 import { FiSearch } from "react-icons/fi";
+import { IoIosArrowBack } from "react-icons/io";
+import { MdPersonSearch } from "react-icons/md";
+import { IoCreateOutline } from "react-icons/io5";
 
 
 function EventPage(){
     return (
       <div className="eventpage-container flex">
         <div className="search-container">
-        <input type="text" placeholder="Search" className="search" />
-        <FiSearch className="search-icon" />
-      </div>
-        <div className="table flex">
-            <div className="table-title">Create an Account</div>
-            <div className="lable">
-                Email
-            </div>
-            <div className="user-detail">
-                <input type="text" value="" />
-            </div>
-            <div className="lable">
-                Password
-            </div>
-            <div className="user-detail">
-                <input type="text" value="Password must contain at least 8 characters" className="password-hint" />
-            </div>
-            <div className="lable">
-                Confirm Password
-            </div>
-            <div className="user-detail">
-                <input type="text" value="" />
-            </div>
-            <button className="btn">Sign up</button>
+            <div className="back"><IoIosArrowBack /></div>
+            <input type="text" placeholder="Search" className="search" />
+            <FiSearch className="search-icon" />
         </div>
-        <div>
-            <div>------------------Or------------------</div>
-            <div>Continue with Facebook</div>
+        <div className="join">
+            <div>
+                <p>Discover Events to<br /> meet new people!</p>
+                <button>Join Events</button>
+            </div>
+            <MdPersonSearch className="person-search" />
+        </div>
+        <div className="create">
+            <IoCreateOutline className="create-event"/>
+            <div>
+                <p>Create Events to<br />invite your friends!</p>
+                <button>Create Events</button>
+            </div>
+        </div>
+        <p>Browse events</p>
+        <div className="browse">
+            <div>
+                <img
+                src="src/Page/EventPage/eventpage_2.png"
+                alt="event1"
+                className="eventpic"
+                />
+                <div>Event 1</div>
+            </div>
+            <div>
+                <img
+                src="src/Page/EventPage/eventpage_2.png"
+                alt="event2"
+                className="eventpic"
+                />
+                <div>Event 2</div>
+            </div>
+            <div>
+                <img
+                src="src/Page/EventPage/eventpage_2.png"
+                alt="event3"
+                className="eventpic"
+                />
+                <div>Event 3</div>
+            </div>
         </div>
 
-        
       </div>
     )
-  }
+}
 
 export default EventPage;
