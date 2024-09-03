a76eb4b542fa8319de4fffe178d1918f62bcaff6
Wai Chun TSANG <wct452@uowmail.edu.au>
2024-09-03 12:48:34 +1000

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,12 @@ import fs from 'fs';
 dotenv.config();
 
 // Decode the certificate by replacing the `\n` with actual newlines
-const sslCert = process.env.DB_SSL_CERT.replace(/\\n/g, '\n');
+const sslCert = process.env.DB_SSL_CERT ? process.env.DB_SSL_CERT.replace(/\\n/g, '\n') : null;
+
+if (!sslCert) {
+    console.error('DB_SSL_CERT environment variable is not set');
+    process.exit(1); // Exit the application with an error code
+}
 
 const db = createPool({
     host: process.env.DB_HOST,
