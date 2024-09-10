a601e6feaab021a6786cf273718b75df2ee88b94
OceanNawshad <reshad947@gmail.com>
2024-09-10 13:19:30 +1000

    Update testingDB.sql

--- Database/testingDB.sql
+++ Database/testingDB.sql
@@ -19,14 +19,14 @@ INSERT INTO customer (id, custname) VALUES
 USE testingDB;
 
 CREATE TABLE events (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  event_name VARCHAR(255) NOT NULL,
-  event_date DATE,
-  customer_id INT,
-  FOREIGN KEY (customer_id) REFERENCES customer(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    latitude DECIMAL(9, 6) NOT NULL,
+    longitude DECIMAL(9, 6) NOT NULL,
+    category ENUM('Hiking', 'Cycling') NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-
-INSERT INTO events (event_name, event_date, customer_id) VALUES
-('hiking at Mount Keira', '2024-09-15', 1),
-('Cycling at Stuart Park', '2024-10-05', 2),
-('Walking at North Touragdi Beach', '2024-11-20', 3);
+INSERT INTO events (name, latitude, longitude) VALUES
+('Event 1', -34.4278, 150.8931),
+('Event 2', -34.4271, 150.8905);
