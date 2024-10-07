40067fe5a5084b6f1bdfbe0cd655b2d9509a7b0d
DarrenIsNotNoob <darren535605@hotmail.com>
2024-10-08 00:19:38 +1100

    fixed css, added picture upload

--- frontend/src/Page/SettingProfile/SettingProfile.tsx
+++ frontend/src/Page/SettingProfile/SettingProfile.tsx
@@ -7,8 +7,24 @@ import { TfiEmail } from "react-icons/tfi";
 import { FaPhoneAlt } from "react-icons/fa";
 import { GoPeople } from "react-icons/go";
 import { RxActivityLog } from "react-icons/rx";
+import { useState } from 'react';
 
 function SettingProfile() {
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+
+  // Function to handle file input change
+  const handleProfilePictureUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && (file.type === 'image/jpeg' || file.type === 'image/jpg')) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfileImage(reader.result as string); // Convert image to base64 and set it
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert("Please upload a valid .jpeg or .jpg file");
+    }
+  };
 
   return (
     <div className="settingprofile-container flex">
@@ -18,30 +34,44 @@ function SettingProfile() {
         </div>
         <h2>Profile Setting</h2>
       </div>
-        <div className="setting">
-            <CgProfile className="icon" />
-            <div>Profile Picture</div>
-        </div>
-        <div className="setting">
-            <CiEdit className="icon" />
-            <div>Full Name</div>
-        </div>
-        <div className="setting">
-            <TfiEmail className="icon" />
-            <div>Email Address</div>
-        </div>
-        <div className="setting">
-            <FaPhoneAlt className="icon" />
-            <div>Contact Number</div>
-        </div>
-        <div className="setting">
-            <GoPeople className="icon" />
-            <div>Friend Network</div>
-        </div>
-        <div className="setting">
-            <RxActivityLog className="icon" />
-            <div>Activities</div>
-        </div>
+      
+      {/* Profile Picture Upload */}
+      <div className="setting" onClick={() => document.getElementById('profile-upload')?.click()}>
+        {profileImage ? (
+          <img src={profileImage} alt="Profile" className="profile-picture" />
+        ) : (
+          <CgProfile className="icon" />
+        )}
+        <div>Profile Picture</div>
+        <input
+          id="profile-upload"
+          type="file"
+          accept=".jpeg,.jpg"
+          style={{ display: 'none' }} // Hide the actual file input
+          onChange={handleProfilePictureUpload}
+        />
+      </div>
+
+      <div className="setting">
+        <CiEdit className="icon" />
+        <div>Full Name</div>
+      </div>
+      <div className="setting">
+        <TfiEmail className="icon" />
+        <div>Email Address</div>
+      </div>
+      <div className="setting">
+        <FaPhoneAlt className="icon" />
+        <div>Contact Number</div>
+      </div>
+      <div className="setting">
+        <GoPeople className="icon" />
+        <div>Friend Network</div>
+      </div>
+      <div className="setting">
+        <RxActivityLog className="icon" />
+        <div>Activities</div>
+      </div>
     </div>
   );
 }
